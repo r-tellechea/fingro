a88f80d248b8a82664c1a31f065536553b4952c8
r-tellechea <r.tellechea.job@gmail.com>
2024-04-23 00:19:09 +0200

    .

--- fingro/fn/ker.py
+++ fingro/fn/ker.py
@@ -3,6 +3,6 @@ import fingro
 def ker(f: fingro.Homomorphism) -> fingro.Subgroup:
 	return fingro.Subgroup(
 		group=f.dom,
-		sub_index=tuple( i for i in range(len(f.dom)) if f.f(i) == 0 ),
+		sub_index=tuple( i for i in range(len(f.dom)) if f.f[i] == 0 ),
 		name=f'ker({f.name})',
 	)
