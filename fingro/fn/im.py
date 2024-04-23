aabc838b10e4df676ea768a303e6337ff9d1660b
r-tellechea <r.tellechea.job@gmail.com>
2024-04-23 13:01:25 +0200

    Bug in hom.im()

--- fingro/fn/im.py
+++ fingro/fn/im.py
@@ -3,6 +3,6 @@ import fingro
 def im(f: fingro.Homomorphism) -> fingro.Subgroup:
 	return fingro.Subgroup(
 		group=f.cod,
-		sub_index=tuple(set(f(i) for i in range(len(f.dom)))),
+		sub_index=tuple(set(f.f[i] for i in range(len(f.dom)))),
 		name=f'im({f.name})',
 	)
