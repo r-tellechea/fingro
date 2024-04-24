714f8a455124e798177d1613575cc21d310bcc4e
r-tellechea <r.tellechea.job@gmail.com>
2024-04-25 00:16:20 +0200

    .

--- fingro/fn/is_subgroup.py
+++ fingro/fn/is_subgroup.py
@@ -1,6 +1,6 @@
 import fingro
 
-def is_subgroup(H: fingro.Group, G: fingro.Group) -> bool:
-	for monomorphism in fingro.compositions.compose_functions.get_monomorphisms(H, G):
+def is_subgroup(subgroup: fingro.Group, group: fingro.Group) -> bool:
+	for monomorphism in fingro.compositions.compose_functions.get_monomorphisms(subgroup, group):
 		return True
 	return False
