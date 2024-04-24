98dc5858edf11acceafb12e736dd4918b7af9df3
r-tellechea <r.tellechea.job@gmail.com>
2024-04-24 17:45:10 +0200

    .

--- fingro/fn/get_subgroup.py
+++ fingro/fn/get_subgroup.py
@@ -2,5 +2,5 @@ import fingro
 
 def get_subgroup(H: fingro.Group, G: fingro.Group) -> fingro.Subgroup | None:
 	for monomorphism in fingro.compositions.compose_functions.get_monomorphisms(H, G):
-		return monomorphism.im()
+		return monomorphism.im
 	return None
