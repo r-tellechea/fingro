ea0cdfa111d1bea97fed9378690af8395fee943b
r-tellechea <r.tellechea.job@gmail.com>
2024-12-04 08:46:46 +0100

    Refactor Test fn.

--- tests/fn/test_get_subgroup.py
+++
@@ -1,9 +0,0 @@
-import fingro
-import numpy as np
-
-def test_get_subgroup():
-	C4 = fingro.families.Cyclic(4)
-	D4 = fingro.families.Dihedral(4)
-	H = fingro.fn.get_subgroup(C4, D4)
-	assert fingro.same_matrix(H.group, D4)
-	assert C4 == H
